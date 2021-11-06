854c43ef51c30cdcadaebdcdb5d14aeb3e68c47b
David del Puerto Martín <ddelpuertomartin@gmail.com>
2021-11-06 19:21:54 +0100

    Created use case to handle users follow requests

---
+++ src/modules/Users/application/sendFollowRequest.js
@@ -0,0 +1,29 @@
+import { Container } from 'typedi';
+import UserNotFound from '../domain/errors/UserNotFound';
+import UserFollowHimself from '../domain/errors/UserFollowHimself';
+import AlreadyRequestedToFollowUser from '../domain/errors/AlreadyRequestedToFollowUser';
+
+const { mongoUsersRepository } = Container.get('repositories');
+
+export default async function loginUser({ requesterUserEmail, identifier }) {
+  const userToFollow = await mongoUsersRepository.findOneByIdOrAccountNumber(identifier);
+
+  if (!userToFollow) {
+    throw new UserNotFound();
+  }
+
+  if (userToFollow.email === requesterUserEmail) {
+    throw new UserFollowHimself();
+  }
+
+  const requesterUser = await mongoUsersRepository.findOneByEmail(requesterUserEmail);
+
+  const followAlreadyRequested = await mongoUsersRepository.followAlreadyRequested({ requesterUser, userToFollow });
+
+  if (followAlreadyRequested) {
+    throw new AlreadyRequestedToFollowUser();
+  }
+
+  userToFollow.connectionRequests.push(requesterUser);
+  await userToFollow.save();
+}
