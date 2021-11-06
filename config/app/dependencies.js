76b4167872735d50706ad2723f51c9387b36dd70
David del Puerto Martín <ddelpuertomartin@gmail.com>
2021-11-06 19:17:52 +0100

    Updated MongoUsersRepository import path

--- config/app/dependencies.js
+++ config/app/dependencies.js
@@ -4,7 +4,7 @@
 
 import Bitnacle from 'bitnacle';
 import UsersModel from '../../src/Shared/infrastructure/persistence/mongo/models/UsersModel';
-import MongoUsersRepository from '../../src/modules/Users/infrastructure/MongoUsersRepository';
+import MongoUsersRepository from '../../src/modules/Users/infrastructure/persistence/MongoUsersRepository';
 
 export default function createDependencies() {
   // Container will load dependencies from this object
