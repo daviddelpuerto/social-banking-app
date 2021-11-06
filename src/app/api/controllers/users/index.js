138cba70f15df0850cb6a7387609a1cb4729e494
David del Puerto Martín <ddelpuertomartin@gmail.com>
2021-11-06 19:23:08 +0100

    Created controller to follow users

--- src/app/api/controllers/users/index.js
+++ src/app/api/controllers/users/index.js
@@ -1,7 +1,9 @@
 import createUserController from './users.create';
+import followUserController from './users.follow';
 import loginUserController from './users.login';
 
 export default {
   createUserController,
+  followUserController,
   loginUserController,
 };
---
+++ src/app/api/controllers/users/users.follow.js
@@ -0,0 +1,34 @@
+import { Container } from 'typedi';
+import httpStatus from 'http-status';
+import sendFollowRequest from '../../../../modules/Users/application/sendFollowRequest';
+import UserNotFound from '../../../../modules/Users/domain/errors/UserNotFound';
+import UserFollowHimself from '../../../../modules/Users/domain/errors/UserFollowHimself';
+import AlreadyRequestedToFollowUser from '../../../../modules/Users/domain/errors/AlreadyRequestedToFollowUser';
+import RoutesResponsesErrorManager from '../../../../Shared/domain/RoutesResponsesErrorManager';
+
+const logger = Container.get('logger');
+
+export default async (req, res) => {
+  try {
+    const { identifier } = req.params;
+    const { decodedToken } = req;
+
+    const requesterUserEmail = decodedToken.payload;
+
+    await sendFollowRequest({ requesterUserEmail, identifier });
+
+    return res.sendStatus(httpStatus.OK);
+  } catch (error) {
+    logger.error(error.stack, { req });
+
+    if (error instanceof UserNotFound) {
+      return res.status(httpStatus.NOT_FOUND).json(RoutesResponsesErrorManager.getResponse(error));
+    } if (error instanceof UserFollowHimself) {
+      return res.status(httpStatus.BAD_REQUEST).json(RoutesResponsesErrorManager.getResponse(error));
+    } if (error instanceof AlreadyRequestedToFollowUser) {
+      return res.status(httpStatus.BAD_REQUEST).json(RoutesResponsesErrorManager.getResponse(error));
+    }
+
+    return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+};
