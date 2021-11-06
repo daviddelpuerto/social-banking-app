0f269d4c250869aab4848b6029e1d677c6d01656
David del Puerto Martín <ddelpuertomartin@gmail.com>
2021-11-06 19:19:44 +0100

    Modified value object to fixed 10 digits since sometimes it was returning 9 digits numbers

--- src/Shared/domain/value-object/AccountNumberValueObject.js
+++ src/Shared/domain/value-object/AccountNumberValueObject.js
@@ -1,6 +1,6 @@
 export default class AccountNumberValueObject {
   constructor() {
     // Used the Math library as it does not need to be cryptographically random
-    this.value = Math.floor(Math.random() * 9000000000) + 1;
+    this.value = parseInt(Math.random().toFixed(10).replace('0.', ''), 10);
   }
 }
