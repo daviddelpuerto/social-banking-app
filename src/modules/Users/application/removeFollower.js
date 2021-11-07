ec1ba8afedaab04a41132303907fe0c86613a38f
David del Puerto Martín <ddelpuertomartin@gmail.com>
2021-11-07 23:36:26 +0100

    Created use case to remove follower

---
+++ src/modules/Users/application/removeFollower.js
@@ -0,0 +1,22 @@
+import { Container } from 'typedi';
+import UserNotFound from '../domain/errors/UserNotFound';
+
+const { mongoUsersRepository } = Container.get('repositories');
+
+export default async function declineFollowRequest({ userEmailRemovingTheRequest, userId }) {
+  const user = await mongoUsersRepository.findById(userId);
+
+  if (!user) {
+    throw new UserNotFound();
+  }
+
+  const userRemovingTheConnection = await mongoUsersRepository.findOneByEmail(userEmailRemovingTheRequest, true);
+
+  userRemovingTheConnection.followers.pull(userId);
+  user.following.pull(userRemovingTheConnection);
+
+  await Promise.all([
+    userRemovingTheConnection.save(),
+    user.save(),
+  ]);
+}
