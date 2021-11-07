8f585cffc468ee7c4f07ad272fb619de15c96197
David del Puerto Martín <ddelpuertomartin@gmail.com>
2021-11-07 13:33:50 +0100

    Created use case to decline user follow request

---
+++ src/modules/Users/application/declineFollowRequest.js
@@ -0,0 +1,18 @@
+import { Container } from 'typedi';
+import UserNotFound from '../domain/errors/UserNotFound';
+
+const { mongoUsersRepository } = Container.get('repositories');
+
+export default async function declineFollowRequest({ userEmailDecliningTheRequest, userId }) {
+  const user = await mongoUsersRepository.findById(userId);
+
+  if (!user) {
+    throw new UserNotFound();
+  }
+
+  const userDecliningTheConnection = await mongoUsersRepository.findOneByEmail(userEmailDecliningTheRequest, true);
+
+  userDecliningTheConnection.connectionRequests.pull(userId);
+
+  await userDecliningTheConnection.save();
+}
