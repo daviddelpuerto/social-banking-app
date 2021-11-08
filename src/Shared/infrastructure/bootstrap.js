6059c2581c687d677098475e9b8f5fb0d7ebb9e4
David del Puerto Martín <ddelpuertomartin@gmail.com>
2021-11-08 15:47:41 +0100

    Removed setInterval for database connection

--- src/Shared/infrastructure/bootstrap.js
+++ src/Shared/infrastructure/bootstrap.js
@@ -4,22 +4,14 @@ import createDatabaseConnection from './persistence/mongo/connection';
 const logger = Container.get('logger');
 
 export default async function bootstrap() {
-  return new Promise((resolve) => {
-    logger.info('Starting application bootstrap process');
-    logger.info(`Detected Node ${process.version}`);
-    const databaseConnectionInterval = setInterval(async () => {
-      try {
-        logger.info('Trying to estabilish database connection...');
-        await createDatabaseConnection();
-        logger.info('Successfully connected to MongoDB database');
-
-        clearInterval((databaseConnectionInterval));
-
-        resolve();
-      } catch (error) {
-        logger.error('There was an error connecting to MongoDB database. Retrying in 5 seconds...');
-        logger.error(error);
-      }
-    }, 5000);
-  });
+  logger.info('Starting application bootstrap process');
+  logger.info(`Detected Node ${process.version}`);
+  try {
+    logger.info('Trying to estabilish database connection...');
+    await createDatabaseConnection();
+    logger.info('Successfully connected to MongoDB database');
+  } catch (error) {
+    logger.error('There was an error connecting to MongoDB database. Retrying in 5 seconds...');
+    logger.error(error);
+  }
 }
